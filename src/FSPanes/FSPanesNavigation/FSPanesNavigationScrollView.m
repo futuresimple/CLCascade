b240ebdf1b0cd4179f86f14833fcfb1ddb02433e
Jacek Łyp <jacek.lyp@getbase.com>
2014-08-06 11:57:50 +0200

    Handling gestures conflict

--- src/FSPanes/FSPanesNavigation/FSPanesNavigationScrollView.m
+++ src/FSPanes/FSPanesNavigation/FSPanesNavigationScrollView.m
@@ -30,7 +30,6 @@
         [self setShowsVerticalScrollIndicator: NO];
         [self setShowsHorizontalScrollIndicator: NO];
         [self setPagingEnabled:NO];
-        
         [self setAutoresizingMask:
          UIViewAutoresizingFlexibleBottomMargin | 
          UIViewAutoresizingFlexibleTopMargin | 
@@ -38,6 +37,18 @@
         
     }
     return self;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
+{
+    //This is very much needed to check if touch.view (on the whole scrollView) has any gestureRecognizers, if it does have, we check if its delegate is kind of tableViewCell class, so we can use swipe gesture to edit cells and disable interfering panes movement. It has to check innerGestureRecognizer.delegate property and not .view property, because cell's swipe gestureRecognizer view is UITableViewCellContentView
+    if ([touch.view.gestureRecognizers count] > 0) {
+        UIGestureRecognizer *innerGestureRecognizer = [touch.view.gestureRecognizers firstObject];
+        if ([innerGestureRecognizer.delegate isKindOfClass:[UITableViewCell class]]) {
+            return NO;
+        }
+    }
+    return YES;
 }
 
 @end
