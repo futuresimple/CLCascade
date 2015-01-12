fa28e6056619170f172c7dd4f52a1272787644df
Jacek Łyp <jacek.lyp@getbase.com>
2015-01-12 16:14:36 +0100

    Get rid of ios6 stuff

--- src/FSPanes/FSPanesSplitView/FSPanesSplitViewController.m
+++ src/FSPanes/FSPanesSplitView/FSPanesSplitViewController.m
@@ -35,16 +35,6 @@
 @synthesize panesMenuViewController = _panesMenuViewController;
 @synthesize panesNavigationController = _panesNavigationController;
 
-+ (BOOL)isRunningIOS6
-{
-    static NSUInteger _deviceSystemMajorVersion = -1;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        _deviceSystemMajorVersion = [[[[[UIDevice currentDevice] systemVersion] componentsSeparatedByString:@"."] objectAtIndex:0] intValue];
-    });
-    return _deviceSystemMajorVersion < 7;
-}
-
 - (NSUInteger)selectedIndex
 {
     return [self.panesMenuViewController.menuDataSource indexOfViewController:self.panesNavigationController.rootViewController];
