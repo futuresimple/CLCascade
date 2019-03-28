05708c40ced62c0ca7207b52657a7dcf83c51eb4
Michal Smaga <michal.smaga@getbase.com>
2019-03-28 09:26:19 +0100

    Create framework target

---
+++ src/FSPanes/Panes/Panes.h
@@ -0,0 +1,18 @@
+//
+//  Panes.h
+//  Panes
+//
+//  Created by Michał Smaga on 27/03/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Panes.
+FOUNDATION_EXPORT double PanesVersionNumber;
+
+//! Project version string for Panes.
+FOUNDATION_EXPORT const unsigned char PanesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Panes/PublicHeader.h>
+
+
