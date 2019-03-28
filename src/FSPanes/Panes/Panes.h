468c82b734079934abf18c8ba3cdc9f03172eaa7
Michal Smaga <michal.smaga@getbase.com>
2019-03-28 10:40:14 +0100

    Expose framework headers

--- src/FSPanes/Panes/Panes.h
+++ src/FSPanes/Panes/Panes.h
@@ -16,3 +16,15 @@ FOUNDATION_EXPORT const unsigned char PanesVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Panes/PublicHeader.h>
 
 
+#import "FSPanesGlobal.h"
+#import "FSPanesProtocols.h"
+#import "UIViewController+FSPanes.h"
+#import "FSPaneView.h"
+#import "FSPanesSplitView.h"
+#import "FSPaneBorderShadowView.h"
+#import "FSPanesSplitViewController.h"
+#import "FSPanesMenuViewController.h"
+#import "FSPanesNavigationView.h"
+#import "FSPanesNavigationScrollView.h"
+#import "FSPanesNavigationController.h"
+#import "FSNavigationMenuDataSource.h"
